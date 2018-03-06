aa47d220c330693e2c403494b9fe5d43f0fb8e9c
Juman Pak <qkrwnaks92@gmail.com>
2018-03-06 12:13:51 +0900

    40. Styleing a Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,28 @@ import { View } from 'react-native';
 
 const Card = () => {
   return (
-    <View></View>
+    <View style={styles.containerStyle}>
+      
+    </View>
   );
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: 'gray',
+    borderBottomWidth: 0,
+    shadowColor: 'white',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+
+  }
 };
 
 export default Card;
