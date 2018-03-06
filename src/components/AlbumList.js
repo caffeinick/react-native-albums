7d192ffbf9c253f05fa11f2886b5a16543f8c38e
Juman Pak <qkrwnaks92@gmail.com>
2018-03-06 11:15:34 +0900

    35. Component Level State

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] };                                                 // 1. define initial state
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
-    })
+    .then((responseData) => this.setState({ albums: responseData })) // 2. define state by setState method
   }
 
   render() {
+    console.log(this.state);
+    
     return (
       <View>
         <Text>Album List!! </Text>
