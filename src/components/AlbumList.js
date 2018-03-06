b8f297a80f08b0796c44c51f7a4b41ca5cc604f8
Juman Pak <qkrwnaks92@gmail.com>
2018-03-06 11:31:16 +0900

    36. More on Component Level State

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  // State is for components internal recordkeeping that is the purpose of state.
+  // We use state whenever we want to update some amount of data overtime.
   state = { albums: [] };                                                 // 1. define initial state
 
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then((response) => response.json())
-    .then((responseData) => this.setState({ albums: responseData })) // 2. define state by setState method
+    .then((responseData) => this.setState({ albums: responseData }))      // 2. update state by this.setState method
   }
 
   render() {
