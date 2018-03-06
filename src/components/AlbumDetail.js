094ce50eaab65520ebf5a843c8dd8091f97f914e
Juman Pak <qkrwnaks92@gmail.com>
2018-03-06 17:35:18 +0900

    47. Displaying Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,36 +7,63 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   // object destructuring
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const { 
+    thumbnailContainerStyle,
+    thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image 
+        <View style={thumbnailContainerStyle}>
+          <Image
             style={thumbnailStyle}
-            source={{ uri: thumbnail_image }} 
+            source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
 };
 
 const styles = {
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
   thumbnailStyle: {
     width: 50,
-    height: 50
+    height: 50,
   },
   headerContentStyle: {
     flexDirection: 'column',
-    justifyContent: 'space-around'
-  }
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  imageStyle: {
+    width: null,
+    height: 300,
+    flex: 1,
+  },
 };
 
 AlbumDetail.propTypes = {
